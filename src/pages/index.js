ae7e4be0c44af938ffa2ca042cfa6444e097db5c
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-27 10:22:31 -0300

    new: add main project pages and basic setup with layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>our home page</h1>
+      <ExampleButton>Example button</ExampleButton>
+    </Layout>
+  )
 }
