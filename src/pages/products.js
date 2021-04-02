e53d38542e65091e02037478f9b20397a3476a33
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-04-02 14:34:56 -0300

    new: added logic and data to display products

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,12 +1,61 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import "./Products.css"
+import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const products = () => {
+const ComponentName = ({ data }) => {
+  const {
+    allContentfulProduct: { nodes: products },
+  } = data
+
   return (
     <Layout>
-      <h1>this is our products page</h1>
+      <section className="page">
+        {products.map(product => {
+          const {
+              id,
+              title,
+              price,
+              slug,
+              image,
+              info: { info },
+            } = product,
+            img = getImage(image)
+
+          return (
+            <article key={id}>
+              <GatsbyImage image={img} alt={title} />
+              <h3>
+                {title}
+                <span> ${price}</span>
+              </h3>
+              <Link to={`/products/${slug}`}>More Details</Link>
+            </article>
+          )
+        })}
+      </section>
     </Layout>
   )
 }
 
-export default products
+export const query = graphql`
+  {
+    allContentfulProduct {
+      nodes {
+        title
+        price
+        slug
+        image {
+          gatsbyImageData(placeholder: BLURRED)
+        }
+        info {
+          info
+        }
+      }
+    }
+  }
+`
+
+export default ComponentName
